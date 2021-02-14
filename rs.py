5d723113236ade2f8bd413a723a3d2f0244891a7
Karun Kanda <kk951@atlas.cs.rutgers.edu>
2021-02-14 15:03:37 -0500

    finalized

--- rs.py
+++ rs.py
@@ -1,79 +1,95 @@
-import time, random, socket
+import time, random, socket, sys
+ 
+'''
+create_rsdns_table does not take any arguments and returns a dictionary of the DNS table found 
+in PROJI-DNSRS.txt.
 
-class RS:
-    hostname = None
-    ip = None 
-    flag = None
-
-    def __init__(self, hostname, ip, flag):
-        self.hostname = hostname
-        self.ip = self.ip
-        self.flag = flag
-
-# creates a 2d array to represent a table type of lookup
-def table_to_string(table):
-    for i in range(0,len(table)):
-        print(table[i].hostname, table[i].ip, table[i].flag)  
-
-# create the rs dns table for the given file
+The purpose of this method is for when the client requests a corresponding IP address for the given Hostname 
+that the RS server can iterate this table and give the client the corresponding column in the table that
+matches with the domain name given or the TS server that might have information on the domain name. 
+'''
 def create_rsdns_table():
-    rsdns_table = [None]*6
+    rsdns_table = []
     index = 0
 
     file = open("PROJI-DNSRS.txt", "r")
     for info in file:
         tokens = info.split()
-        if tokens[0] == '-':
-            tokens[0] = 'None'
-        elif tokens[1] == '-':
-            tokens[1] = 'None'
-        elif tokens[2] == '-':
-            tokens[2] = 'None'
-        rsdns_table[index] = RS(tokens[0], tokens[1], tokens[2])
+        rsdns_table.append((tokens[0], tokens[1], tokens[2]))
         index+=1
     
     return rsdns_table
 
+'''
+check_hostname_table takes in two arguments, the queried hostname from the client and the RS server table
+and returns a column from the table or the last column according to if the hostname is found in the table.
+
+The purpose of this method is to check if given hostname is found in the RS server and if it is not found in 
+this table give it the corresponding TS server that might have information on it. It iterates the table and if 
+a match is found it returns:
+    Hostname IP Address A
+However if a match isn't found it returns the last column of the table which is:
+    localhost - NS
+Where the A should tell the client just to output the results and NS should tell the client that the RS server 
+didn't have the information that it needed so pass the domain name to the corresponding TS server.
+'''
 def check_hostname_table(queried_hostname, table):
     for i in range(0, len(table)):
-        if table[i].hostname == queried_hostname:
-            return '{} {} {}'.format(table[i].hostname, table[i].ip, table[i].flag)
+        if table[i][0].lower() == queried_hostname.lower():
+            return '{} {} {}'.format(table[i][0], table[i][1], table[i][2])
     
-    return 'Information not found'
+    return '{} {} {}'.format(table[len(table)-1][0], table[len(table)-1][1], table[len(table)-1][2])
 
+'''
+rs_server takes in no arguments and doesn't return anything because this is where the RS server is going to 
+start from. Upon start the RS server will create its respective table which will be used when the client connects
+and provides the server a hostname to start with.
 
+The purpose of this method is to hold the main logic behind the RS server upon connection. First creating its RS
+table. Then waiting for a response from the client to give it a match or a given TS server to connect to next. 
+The socket used to create the RS server will contain a reusable address so that the client can query everything in
+its table.
+'''
 def rs_server():
     # initial setup for the server side
-    try:
-        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("[S]: Server socket created")
-    except socket.error as err:
-        print('socket open error: {}\n'.format(err))
-        exit()
+    while True:
+        try:
+            ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            print("[S]: Server socket created")
+        except socket.error as err:
+            print('socket open error: {}\n'.format(err))
+            exit()
 
-    server_binding = ('', 1903)
-    ss.bind(server_binding)
-    ss.listen(1)
-    host = socket.gethostname()
-    localhost_ip = (socket.gethostbyname(host))
-    csockid, addr = ss.accept()
-    print ("[S]: Got a connection request from a client at {}".format(addr)) # check if we recieved a connection or not
+        server_binding = ('', int(sys.argv[1]))
+        ss.bind(server_binding)
+        ss.listen(1)
+        host = socket.gethostname()
+        localhost_ip = (socket.gethostbyname(host))
+        csockid, addr = ss.accept()
+        print ("[S]: Got a connection request from a client at {}".format(addr)) # check if we recieved a connection or not
 
-    # create the rs dns table for lookup
-    table = create_rsdns_table()
+        # create the rs dns table for lookup
+        table = create_rsdns_table()
 
-    # for testing, let's see what we get back
-    queried_hostname = csockid.recv(1024)
-    print('[S]: Server recieved {} from the client'.format(queried_hostname))
+        # for testing, let's see what we get back
+        queried_hostname = csockid.recv(1024)
 
-    # next part is the table lookup to give the client its respective string
-    queried_string = check_hostname_table(queried_hostname, table)
-    csockid.send(queried_string.encode('utf-8'))
+        # next part is the table lookup to give the client its respective string
+        queried_string = check_hostname_table(queried_hostname, table)
+        csockid.send(queried_string.encode('utf-8'))
 
     ss.close()
     exit()
 
 
+'''
+This is the main method where the program will start. rs.py is expecting two arguments one being 
+python rs.py and the port number so it can start successfully.
+'''
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Incorrect Usage: python rs.py <port number>")
+        sys.exit(1)
     rs_server()
     
--- ts.py
+++ ts.py
@@ -1,35 +1,99 @@
-import time, random, socket
+import time
+import random
+import socket, sys
 
-class TS:
-    hostname = None
-    ip = None 
-    flag = None
+'''
+create_tsdns_table takes in no arguments and returns a dictionary containing the information found in 
+PROJI-DNSTS.txt.
 
-    def __init__(self, hostname, ip, flag):
-        self.hostname = hostname
-        self.ip = self.ip
-        self.flag = flag
+The purpose of this method is to create a table that the TS server can iterate through to provide the connected
+client the information its looking for. Otherwise if the information can't be found it will be give it an
+error and send the error string back to the client.
+'''
+def create_tsdns_table():
+    tsdns_table = []
+    index = 0
 
+    file = open("PROJI-DNSTS.txt", "r")
+    for info in file:
+        tokens = info.split()
+        tsdns_table.append((tokens[0], tokens[1], tokens[2]))
+
+    return tsdns_table
+
+'''
+check_hostname_table takes in two arguments queried_hostname and table (the table containing the information 
+created in the method create_tsdns_table()) and returns a column from the table or an error if the hostname couldn't
+be found.
+
+The purpose this table is to check if the given hostname can be found in the TS server otherwise if it can't
+be found in the TS table just provide it a Error string that the client will recieve. If a match is found in this server
+it will do return a similar result like in RS server returning back:
+    Hostname IP Address A
+Otherwise if a match can not be found it will return the following error string:
+    Hostname - Error:HOST NOT FOUND
+Where no matter the results the client will print the respective string back so the user can interpet the
+results to their liking.
+'''
+def check_hostname_table(queried_hostname, table):
+    for i in range(0, len(table)):
+        if table[i][0].lower() == queried_hostname.lower():
+            return '{} {} {}'.format(table[i][0], table[i][1], table[i][2])
+
+    return '{} - Error:HOST NOT FOUND'.format(queried_hostname)
+
+'''
+ts_server takes in no arguments and returns nothing because this is where the main logic of the TS server will lie.
+Upon start the TS server will create a table that will be used when the client requests information from the server 
+upon connecting to it.
+
+The purpose of the method is to house the logic of the TS server. First the table will be created but unlike the RS server
+the TS server has a possibility of never being connected to so there is a chance that the TS server will continuely be waiting
+for a client to connect to it. Upon connection the given hostname is compared to the table given and provide it the information that
+it is looking for otherwise give the client an error message. Like the RS server, the socket used to create the server
+will be resusable so in case more clients would like to request information.
+'''
 def ts_server():
     # initial setup for the server side
-    try:
-        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("[S]: Server socket created")
-    except socket.error as err:
-        print('socket open error: {}\n'.format(err))
-        exit()
+    while True:
+        try:
+            ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            print("[S]: Server socket created")
+        except socket.error as err:
+            print('socket open error: {}\n'.format(err))
+            exit()
 
-    server_binding = ('', 1903)
-    ss.bind(server_binding)
-    ss.listen(1)
-    host = socket.gethostname()
-    localhost_ip = (socket.gethostbyname(host))
-    csockid, addr = ss.accept()
-    print ("[S]: Got a connection request from a client at {}".format(addr)) # check if we recieved a connection or not
+        server_binding = ('', int(sys.argv[1]))
+        ss.bind(server_binding)
+        ss.listen(1)
+        host = socket.gethostname()
+        localhost_ip = (socket.gethostbyname(host))
+        csockid, addr = ss.accept()
+        # check if we recieved a connection or not
+        print("[S]: Got a connection request from a client at {}".format(addr))
+
+        # setting up the dns table on the TS server side
+        table = create_tsdns_table()
+
+        # get the input from the client
+        queried_hostname = csockid.recv(1024)
+
+        # next TS should look into its respective table and send what it finds back to the client
+        queried_string = check_hostname_table(queried_hostname, table)
+        csockid.send(queried_string.encode('utf-8'))
 
     ss.close()
     exit()
 
-
+'''
+This is the main method of ts.py where ts.py is expecting two arguments to start (python ts.py) and (the portnumber)
+to start successfully.
+'''
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Incorrect Usage: python ts.py <port number>")
+        sys.exit(1)
     ts_server()
+
